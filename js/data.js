5be09f0bdeebbb945c8d999f2d09c2b6ecfab291
Peter Massini <peter@massini.xyz>
2020-01-29 20:14:40 +0100

    new thingy

---
+++ js/data.js
@@ -0,0 +1,34 @@
+
+var headerText = "BS-Bingo";
+var footerText = "";
+var winText = "Winner";
+var clickSnd = new Audio("audio/click.mp3");
+var winSnd = new Audio("audio/win.mp3");
+
+var JSONBingo = {"squares": [
+        {"square": "uebergang verkackt"},
+        {"square": "Pirat"},
+        {"square": "Mann mit fischernetz Top"},
+        {"square": "Muffin Hose"},
+        {"square": "Zu enges Korest"},
+        {"square": "Musik hoert auf zu spielen"},
+        {"square": "gehstock mit komischer figur"},
+        {"square": "es richt komisch"},
+        {"square": "uebergang verkackt"},
+        {"square": "Pirat"},
+        {"square": "Mann mit fischernetz Top"},
+        {"square": "Muffin Hose"},
+        {"square": "Zu enges Korest"},
+        {"square": "Musik hoert auf zu spielen"},
+        {"square": "gehstock mit komischer figur"},
+        {"square": "es richt komisch"},
+        {"square": "uebergang verkackt"},
+        {"square": "Pirat"},
+        {"square": "Mann mit fischernetz Top"},
+        {"square": "Muffin Hose"},
+        {"square": "Zu enges Korest"},
+        {"square": "Musik hoert auf zu spielen"},
+        {"square": "gehstock mit komischer figur"},
+        {"square": "es richt komisch"},
+    ]
+};
